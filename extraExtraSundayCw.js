6709ad19251f4411d495ca3d7924d6aa48efce44
zikrehaimanot <zikrehaimanot@gmail.com>
2020-03-01 18:02:32 -0500

    final sunday CW

---
+++ extraExtraSundayCw.js
@@ -0,0 +1,23 @@
+// Write function bmi that calculates body mass index (bmi = weight / height ^ 2).
+//
+// if bmi <= 18.5 return "Underweight"
+//
+// if bmi <= 25.0 return "Normal"
+//
+// if bmi <= 30.0 return "Overweight"
+//
+// if bmi > 30 return "Obese"
+
+
+
+
+
+
+function bmi(weight, height) {
+  var bm = weight / (height * height);
+  if (bm <= 18.5) return "Underweight"
+  if (bm <= 25) return "Normal"
+  if (bm <= 30) return "Overweight"
+  if (bm > 30) return "Obese"
+
+}
