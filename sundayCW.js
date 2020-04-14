643e964e0d440712f20ca85d88f77548091755a2
Zikre Haimanot <60019172+zikrehaimanot@users.noreply.github.com>
2020-04-14 14:28:51 -0400

    Delete sundayCW.js

--- sundayCW.js
+++
@@ -1,7 +0,0 @@
-// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor.
-// First argument is an array of numbers and the second is the divisor.
-//
-
-function divisibleBy(numbers, divisor){
-return numbers.filter (numbers => numbers % divisor === 0) 
-}
